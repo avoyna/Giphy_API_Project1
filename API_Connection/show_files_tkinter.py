1b4ace2389b48591b3f57b5d55c9afb708fce093
Andriy Voyna <andrey.voyna@gmail.com>
2022-07-02 02:24:21 +0300

    Image opening issues fixed

--- API_Connection/show_files_tkinter.py
+++ API_Connection/show_files_tkinter.py
@@ -16,7 +16,7 @@ def find_image_ext(full_url):
     return found
 
 def image_canvas_clicked(event, img_fn):
-    #print("Data^", img_fn, event.x, event.y, event.widget)
+    #print("Data:", img_fn, event.x, event.y, event.widget)
     #img = Image.open(img_fn)
     #img.show()
     os.startfile(img_fn)
@@ -40,7 +40,7 @@ def show_giphy_image(urls_to_show):
         image_id = canvas.create_text( (8, int(pic_height)-10), text=image_fn)
 
         canvas.tag_bind("canvas_image", "<Button-1>", lambda event,
-                                                             img_fn=image_fn: image_canvas_clicked(event, img_fn))
+                                                             img_fn=img_path: image_canvas_clicked(event, img_fn))
         #Label(root, image=imgs[-1], width=pic_width, height=pic_height).grid()
 
     root.call('wm', 'attributes', '.', '-topmost', True)
